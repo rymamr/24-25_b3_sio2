01e64e206e399bb64ef44cedd311de61b9f21d8b
Ryma <rymaraima@gmail.com>
2024-11-29 16:15:58 +0100

    test vendredi

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RoleController;
-
+ 
 Route::get('/', function () {
     return view('welcome');
 });
